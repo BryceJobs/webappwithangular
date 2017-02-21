51282e2438d7f1ab39608e2151190992066370b1
Bryce Jobs <lecebri@gmail.com>
2017-02-22 00:46:02 +0100

    finalizing assingment 3

--- week 3/scripts/app.js
+++ week 3/scripts/app.js
@@ -1,1 +1,77 @@
+(function () {
+	
 
+angular.module("NarrowItDownApp", [])
+
+.constant("baseURL", "http://davids-restaurant.herokuapp.com")
+
+.service("MenuSearchService", ['$http', 'baseURL', function($http, baseURL){
+
+	this.getMatchedMenuItems = function (searchTerm) {
+		
+		return $http({
+
+					method: 'GET',
+					url: baseURL + "/menu_items.json"
+
+				})
+				.then(function(response) {
+					var foundItems = [];
+
+					var results = response.data.menu_items;
+					console.log(results);
+					
+					for (var i = results.length - 1; i >= 0; i--) {
+						if(results[i].description.indexOf(searchTerm) != -1 )
+						{
+							foundItems.push(results[i]);
+						}
+					}
+					
+					return foundItems;
+				});
+
+	}
+
+}])
+
+.controller("NarrowItDownController", ['MenuSearchService', function (MenuSearchService) {
+
+		var search_controller = this;
+
+		search_controller.found = [];
+
+		search_controller.search = function () {
+			var promise = MenuSearchService.getMatchedMenuItems(this.searchTerm);
+			console.log(promise);
+			promise.then(
+				function (response) {
+					search_controller.found = response;
+
+			}
+		);
+		
+	};
+
+	search_controller.removeItem = function (index) {
+		search_controller.found.splice(index, 1);
+	}
+
+
+}])
+
+.directive("foundItems", function () {
+	var ddo = {
+		templateUrl: 'founds.html',
+		restrict: 'E',
+		scope: {
+			foundItems: '<',
+			onRemove: '&'
+		}
+	};
+	return ddo;
+})
+;
+
+
+})();
